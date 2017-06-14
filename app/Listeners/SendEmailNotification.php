c497303fe7fa3d9617198fd2711c8d69d638e05d
mushoodEsokia <my@esokia.com>
2017-06-14 16:09:56 +0400

    Should be functionning now

--- app/Listeners/SendEmailNotification.php
+++ app/Listeners/SendEmailNotification.php
@@ -7,6 +7,7 @@ use App\Events\NewMessage as MessageEvent;
 use App\Mail\NewMessage as MessageMail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Message;
 
 class SendEmailNotification
 {
@@ -30,7 +31,31 @@ class SendEmailNotification
     {
         //hanlde event when a new message has been created
         $message = $event->getMessage();
-        Mail::to('my@esokia-webagency.com')->send(new MessageMail());
+        $id = $message -> id;
+        $replyTo = "platimuswaddimus+".$id."@gmail.com";
+        
+        //getting email address to reply back
+        $mailto = $message->to;
+        $initialMessageId = explode("+", $mailto);
+        
+        if(count($initialMessageId)>1){
+            $initialMessageIdPart = explode("@", $initialMessageId[1]);
+            $initialMessage = Message::find($initialMessageIdPart[0]);
+            //var_dump($initialMessage->from);die();
+            $messageMaillable = new MessageMail();
+            $messageMaillable ->replyTo($replyTo, 'Reply Guy');
+            Mail::to($initialMessage->from)
+                    ->send($messageMaillable);
+        } else{
+            $messageMaillable = new MessageMail();
+            $messageMaillable ->replyTo($replyTo, 'Reply Guy');
+            Mail::to('my@esokia-webagency.com')
+                    ->send($messageMaillable);
+        }
+        
+        
+        
+        
         //dump($message->from);die();
     }
 }
