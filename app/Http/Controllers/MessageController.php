05a2d9a46407799caa0048db9bc6f10392b032a6
mushoodEsokia <my@esokia.com>
2017-06-14 11:33:26 +0400

    Playing with email download and saving to database

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Webklex\IMAP\Client;
+use App\Message;
 
 class MessageController extends Controller
 {
     public function index(){
+        
+        //get count of all message from our database
+        $messagesInDatabase = Message::count();
+        echo $messagesInDatabase . "<br />";
 
         $oClient = new Client([
             'host'          => 'imap.gmail.com',
@@ -23,24 +28,44 @@ class MessageController extends Controller
 
         //Get all Mailboxes
         $aMailboxes = $oClient->getFolders();
+        echo $oClient -> countRecentMessages().'<br />';
+        echo $oClient -> countMessages().'<br />';
+        $messagesInMailbox = $oClient -> countMessages();
        
         //Loop through every Mailbox
         /** @var \Webklex\IMAP\Folder $oMailbox */
         foreach($aMailboxes as $oMailbox){
-            echo $oMailbox -> fullName;
-
-            //Get all Messages of the current Mailbox
-            /** @var \Webklex\IMAP\Message $oMessage */
-            foreach($oMailbox->getMessages() as $oMessage){
-                echo $oMessage->subject.'<br />';
-                echo 'Attachments: '.$oMessage->getAttachments()->count().'<br />';
-                echo $oMessage->getHTMLBody(false);
-
-                //Move the current Message to 'INBOX.read'
-                if($oMessage->moveToFolder('INBOX.read') == true){
-                    echo 'Message has ben moved';
-                }else{
-                    echo 'Message could not be moved';
+            echo $oMailbox -> fullName .'<br />';
+            if($oMailbox -> fullName == 'INBOX'){
+                //Get all Messages of the current Mailbox
+                /** @var \Webklex\IMAP\Message $oMessage */
+                $index = 0;
+                $criteria = 'ALL'; //get all messages in inbox
+                $criteria = 'UNSEEN'; //get only unread messages in inbox
+                foreach($oMailbox->getMessages($criteria) as $key => $oMessage){
+                    $index++;
+                    echo $oMessage->subject.'<br />';
+                    $from = $oMessage->from;
+                    //print_r($from);
+                    echo $from[0]->mail.'<br />';
+                    $to = $oMessage->to;
+                    //print_r($to);
+                    echo $to[0]->mail.'<br />';
+                    //echo 'Attachments: '.$oMessage->getAttachments()->count().'<br />';
+                    echo $oMessage->getHTMLBody(true);
+                    echo "key: " . $index . " <br />";
+                    if($index>$messagesInDatabase){
+                        echo "you have one new message <br />";
+                        
+                        $message = new Message();
+                        $message->from = $from[0]->mail;
+                        $message->to = $to[0]->mail;
+                        $message->subject = $oMessage->subject;
+                        $message->content = $oMessage->getHTMLBody(true);
+                        $message->save();
+                        dump($oMessage);
+                         
+                    }
                 }
             }
         }
